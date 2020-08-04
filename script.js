3491e7497eff269fe405c13eee0c5abf3ef8b36f
IInterface <github.interface@gmail.com>
2020-08-04 12:36:11 -0300

    acertando detalhes

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let food = {
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-function criarBG() {
+function criarBG(){
     context.fillStyle = "lightgreen"
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
@@ -33,14 +33,14 @@ function drawFood(){
 document.addEventListener("keydown", update);
 
 function update(event){
-    if(event.keyCode == 37 && direction != "right") direction = " left";
+    if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != " up") direction = "down";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
     
 
-function iniciarJogo() {    
+function iniciarJogo(){    
     if(snake[0].x > 15 * box && direction == "right" ) snake[0].x = 0;
     if(snake[0].x < 0  && direction == "left" ) snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down" ) snake[0].y = 0;
